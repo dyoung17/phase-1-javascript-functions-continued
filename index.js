68bbccf69325d105afb2e0d90ee9477f9fba136c
Daven Young <davenyoung@msn.com>
2022-04-06 10:51:05 -0600

    commit tree ea265f6f6b4021c83cf7831b6cb9e2f743f922e0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(fun='roller-skate'){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`;
+}
+
+
+function wrapAdjective(emphatic) {
+    
+    function addFlair(flair = '*') {
+        return `You are ${flair} ${emphatic} ${flair}`
+    }
+
+    const result = addFlair();
+
+    console.log(result)
+}
+
+
